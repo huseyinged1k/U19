e226bb853c6753bd25b65ad82f8c8572327176c3
Hüseyin Gedik <gedikh8@gmail.com>
2023-04-09 01:05:41 +0300

    basit kamera takip scripti

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float followSpeed = 0.1f;
+    public float camRange;    
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        camRange = transform.position - player.position;
+    }
+
+    
+    void LateUpdate()
+    {
+        Vector3 targetLocation = player.position + camRange;
+        transform.position = Vector3.Lerp(transform.position, targetLocation, followSpeed);
+    }
+}
