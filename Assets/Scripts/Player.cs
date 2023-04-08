02f92e91b29ae12d589511d9c0e16538258ebd2b
Hüseyin Gedik <gedikh8@gmail.com>
2023-04-08 17:49:52 +0300

    Player ve suruklenebilir objeler icin script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Abilities")]
+    public bool isTelekinetic;
+
+    [SerializeField] private float moveSpeed = 5f;  
+    [SerializeField] private float jumpForce = 10f; 
+    [SerializeField] private float shrinkScale = 0.5f;  
+    [SerializeField] private float growScale = 2f; 
+
+    private Rigidbody2D rb; 
+    private bool isGrounded = false; 
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        Vector2 moveVelocity = rb.velocity;
+        moveVelocity.x = moveInput * moveSpeed;
+
+        rb.velocity = moveVelocity;
+
+        if (Input.GetButtonDown("Jump") && isGrounded) rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        if (Input.GetKeyDown(KeyCode.Q)) transform.localScale *= shrinkScale;
         
+        if (Input.GetKeyDown(KeyCode.E)) transform.localScale *= growScale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) isGrounded = false;
     }
 }
